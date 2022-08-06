8b549e985800d64cac30bc70f766b9e8e0fb8c35
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-08-06 18:41:12 +0300

    created 3d scroll engine that works like slider

--- js/app.js
+++ js/app.js
@@ -2,11 +2,29 @@ import { isWebp } from './components/isWebp.js'
 
 isWebp()
 
-const zSpacing = -1500
+const $frames = Array.from(document.querySelectorAll('.slider__frame'))
+const $html = document.querySelector('html')
+
+const zSpacing = -20000
+const transition = 1500
 let lastPosition = 0
 let zValues = []
 
-const $frames = Array.from(document.querySelectorAll('.frame'))
+const bodyHeight = ($frames.length - 1) * Math.abs(zSpacing)
+
+function throttle(callback, timeout) {
+	let timer = null
+
+	return function perform(...args) {
+		if (timer) return
+
+		callback(...args)
+		timer = setTimeout(() => {
+			clearTimeout(timer)
+			timer = null
+		}, timeout)
+	}
+}
 
 function resetScrollAfterReload(params) {
 	setTimeout(() => {
@@ -17,25 +35,28 @@ function resetScrollAfterReload(params) {
 function initFramesZPositions() {
 	$frames.forEach((frame, index) => {
 		zValues.push(index * zSpacing)
+		console.log(zValues[index] === 0)
+		if (zValues[index] === 0) {
+			frame.style.cssText = `opacity: 1; transform: translateZ(${zValues[index]}px)`
+			return
+		}
 		frame.style.cssText = `transform: translateZ(${zValues[index]}px);`
 	})
 }
 
-function setBodyHeight() {
-	const bodyHeight = ($frames.length - 1) * Math.abs(zSpacing)
-	const html = document.querySelector('html')
-	html.style.setProperty('--body-height', `${bodyHeight}px`)
+function setCssVars() {
+	$html.style.setProperty('--body-height', `${bodyHeight}px`)
+	$html.style.setProperty('--transition-duration', `${transition / 1000}s`)
 }
 
 function updateFramesZPosition(delta) {
 	$frames.forEach((frame, index) => {
 		zValues[index] += delta * -2
-		console.log(zValues[index], delta)
 		frame.style.cssText = `transform: translateZ(${zValues[index]}px);`
 	})
 }
 
-setBodyHeight()
+setCssVars()
 resetScrollAfterReload()
 initFramesZPositions()
 
@@ -44,7 +65,22 @@ window.addEventListener('scroll', () => {
 	let delta = lastPosition - top
 
 	lastPosition = top
-	updateFramesZPosition(delta)
+	// updateFramesZPosition(delta)
+
+	throttledChangeFrame(delta)
 })
 
-console.log(document.documentElement.scrollTop)
+const throttledChangeFrame = throttle(changeFrame, transition)
+
+function changeFrame(delta) {
+	if (zValues[0] === 0 && delta > 0) return
+	if (zValues[zValues.length - 1] === 0 && delta < 0) return
+	$frames.forEach((frame, index) => {
+		delta < 0 ? (zValues[index] -= zSpacing) : (zValues[index] += zSpacing)
+		if (zValues[index] === 0) {
+			frame.style.cssText = `opacity: 1; transform: translateZ(${zValues[index]}px)`
+			return
+		}
+		frame.style.cssText = `transform: translateZ(${zValues[index]}px);`
+	})
+}

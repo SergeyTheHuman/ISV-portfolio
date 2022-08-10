0185326955104affc9bcb39f5a4c42dfabce9895
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-08-10 19:44:01 +0300

    created dynamic popup on portfolio page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 import { isWebp } from './utils/isWebp.js'
 import './components/scroll-3d.js'
 import Accordion from 'accordion-js'
-
-const $cardsCols = document.querySelectorAll('.slide__cards-col')
-const $technologyCards = document.querySelectorAll('.card-technology')
-
-$cardsCols.forEach((el) => {
-	const accordion = new Accordion(el, {
-		duration: 300,
-		showMultiple: false,
-	})
-})
+import { getRandomInteger } from './utils/utils.js'
+import { websitesData } from './websites/websites-data.js'
 
 isWebp()
+
+// Accordions init
+const $cardsCols = document.querySelectorAll('.slide__cards-col')
+
+if ($cardsCols) {
+	$cardsCols.forEach((el) => {
+		const accordion = new Accordion(el, {
+			duration: 300,
+			showMultiple: false,
+		})
+	})
+}
+
+// "Bubbles" logic
+const $technologyCards = document.querySelectorAll('.card-technology')
 
 let borderRadiusProps = []
 let borderRadiusPropsInvert = []
@@ -20,7 +27,7 @@ let borderRadiusPropsInvert = []
 function getRandomBorderRadius(el) {
 	let borderRadiusValues = []
 	for (let i = 0; i < 8; i++) {
-		borderRadiusValues.push(Math.round(Math.random() * 100))
+		borderRadiusValues.push(getRandomInteger(20, 80))
 	}
 	let borderRadius = `border-radius: ${borderRadiusValues[0]}% ${borderRadiusValues[1]}% ${borderRadiusValues[2]}% ${borderRadiusValues[3]}% / ${borderRadiusValues[4]}% ${borderRadiusValues[5]}% ${borderRadiusValues[6]}% ${borderRadiusValues[7]}%`
 	let borderRadiusInvert = `border-radius: ${100 - borderRadiusValues[0]}% ${100 - borderRadiusValues[1]}% ${100 - borderRadiusValues[2]}% ${
@@ -33,12 +40,76 @@ function getRandomBorderRadius(el) {
 	el.style.cssText = borderRadius
 }
 
-$technologyCards.forEach((el, idx) => {
-	getRandomBorderRadius(el)
-	el.addEventListener('mouseenter', () => {
-		el.style.cssText = borderRadiusPropsInvert[idx]
+if ($technologyCards) {
+	$technologyCards.forEach((el, idx) => {
+		getRandomBorderRadius(el)
+		el.addEventListener('mouseenter', () => {
+			el.style.cssText = borderRadiusPropsInvert[idx]
+		})
+		el.addEventListener('mouseleave', () => {
+			el.style.cssText = borderRadiusProps[idx]
+		})
 	})
-	el.addEventListener('mouseleave', () => {
-		el.style.cssText = borderRadiusProps[idx]
+}
+
+// modals logic on portfolio page
+const $websiteCards = document.querySelectorAll('.portfolio__item')
+const $websiteModal = document.querySelector('.website-modal')
+const $websiteModalOverlay = document.querySelector('.modal-overlay')
+let isModalOpened = false
+if ($websiteCards && $websiteModal) {
+	document.addEventListener('click', (e) => {
+		if (e.target.closest('.portfolio__item')) {
+			$websiteModal.classList.add('modal--opened')
+			$websiteModalOverlay.classList.add('modal--opened')
+		}
+		if (e.target.closest('.website-modal__cross') || e.target.closest('.modal-overlay')) {
+			$websiteModal.classList.remove('modal--opened')
+			$websiteModalOverlay.classList.remove('modal--opened')
+		}
 	})
-})
+}
+
+// ===
+if (window.location.pathname === '/') {
+	console.log('Вы на главной')
+} else if (window.location.pathname.includes('/page-portfolio.html')) {
+	console.log('Вы на странице портфолио')
+}
+
+function renderWebsiteModal(websiteInfo, modal) {
+	try {
+		const $modalTitle = modal.querySelector('.website-modal__title')
+		const $modalCategory = modal.querySelector('.website-modal__category')
+		const $modalDescription = modal.querySelector('.website-modal__descr')
+		const $modalTechnologies = modal.querySelector('.website-modal__technologies')
+		const $modalPages = modal.querySelector('.website-modal__pages')
+		const $modalPagesList = modal.querySelector('.website-modal__list')
+		const $modalImage = modal.querySelector('.website-modal__img')
+
+		$modalTitle.textContent = websiteInfo.title
+		$modalCategory.textContent = websiteInfo.category
+		$modalDescription.textContent = websiteInfo.description
+		$modalTechnologies.textContent = websiteInfo.technologies
+		$modalPages.insertAdjacentText('afterbegin', `Количество страниц - ${websiteInfo.pages.length}`)
+		$modalPagesList.innerHTML = getPagesListHTML(websiteInfo.pages)
+		$modalImage.setAttribute('src', `images/websites/${websiteInfo.image}`)
+		$modalImage.setAttribute('alt', `${websiteInfo.image}`)
+	} catch (error) {
+		alert(error)
+	}
+
+	function getPagesListHTML(linksInfo) {
+		let html = ``
+		for (const link of linksInfo) {
+			html += `
+			<li class="website-modal__li">
+				<a class="website-modal__link" href="${link.link}" data-hover-name="${link.name}">${link.name}</a>
+			</li>
+			`
+		}
+		return html
+	}
+}
+
+renderWebsiteModal(websitesData.smartShop, $websiteModal)
---
+++ js/websites/websites-data.js
@@ -0,0 +1,105 @@
+export const websitesData = {
+	smartShop: {
+		title: 'Интернет-магазин техники Smart Shop',
+		category: 'Лэндинг',
+		description:
+			'Нужно было сверстать интернет-магазин по макету в Figma, адаптировать под все устройства. Большой макет со множеством UI-элементов: модальные окна, слайдеры, ползунки с ценой, кастомизированные чекбоксы и радио-кнопки.',
+		technologies:
+			'В работе использовались: препроцессор Sass, шаблонизатор Pug, сборка на Gulp + Webpack для javascript, SVG-спрайт. Наименование классов по БЭМ',
+		pages: [
+			{
+				name: `Главная`,
+				link: `index.html`,
+			},
+			{
+				name: `Каталог`,
+				link: `page-catalog.html`,
+			},
+			{
+				name: `О компании`,
+				link: `page-about-us.html`,
+			},
+			{
+				name: `Корзина`,
+				link: `page-cart.html`,
+			},
+			{
+				name: `Сравнение товаров`,
+				link: `page-compare.html`,
+			},
+			{
+				name: `Контакты`,
+				link: `page-contacts.html`,
+			},
+			{
+				name: `Дропшиппинг`,
+				link: `page-dropshipping.html`,
+			},
+			{
+				name: `Опт`,
+				link: `page-wholesale.html`,
+			},
+			{
+				name: `Избранные`,
+				link: `page-favorite.html`,
+			},
+			{
+				name: `Гарантии`,
+				link: `page-garanty.html`,
+			},
+			{
+				name: `Новости`,
+				link: `page-news.html`,
+			},
+			{
+				name: `Одна новость`,
+				link: `index.html`,
+			},
+			{
+				name: `Рассрочка`,
+				link: `page-installment.html`,
+			},
+			{
+				name: `Страница продукта`,
+				link: `page-product-one.html`,
+			},
+			{
+				name: `Акции`,
+				link: `page-promos.html`,
+			},
+			{
+				name: `Одна акция`,
+				link: `page-promos-one.html`,
+			},
+			{
+				name: `Вакансии`,
+				link: `page-vacancies.html`,
+			},
+			{
+				name: `Просмотренные`,
+				link: `page-viewed.html`,
+			},
+			{
+				name: `Личный кабинет - главная`,
+				link: `page-account-main.html`,
+			},
+			{
+				name: `Личный кабинет - избранные`,
+				link: `page-account-favorite.html`,
+			},
+			{
+				name: `Личный кабинет - смена пароля`,
+				link: `page-account-pass.html`,
+			},
+			{
+				name: `Личный кабинет - персональные данные`,
+				link: `page-account-personal.html`,
+			},
+			{
+				name: `Личный кабинет - история`,
+				link: `page-account-history.html`,
+			},
+		],
+		image: 'smart-shop.png',
+	},
+}

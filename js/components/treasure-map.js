5966b8dc9594f392319f458caa2c63566d0ebd2d
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-08-14 17:46:28 +0300

    improved engine

--- js/components/treasure-map.js
+++ js/components/treasure-map.js
@@ -17,7 +17,11 @@ if (window.location.pathname === '/page-about.html' && !isMobile() && window.inn
 	let mouseXVelocity = 0
 	let mouseYVelocity = 0
 
+	let isPaused = false
+
 	const speed = 25
+
+	translateIntoView(document.querySelector('.about-greetings'))
 
 	document.addEventListener('mousemove', (e) => {
 		windowCenterX = window.innerWidth / 2
@@ -28,14 +32,33 @@ if (window.location.pathname === '/page-about.html' && !isMobile() && window.inn
 
 		mouseXVelocity = Math.abs(mouseX / window.innerWidth)
 		mouseYVelocity = Math.abs(mouseY / window.innerHeight)
+
+		const $frames = document.querySelector('.about__frame')
 	})
 	document.addEventListener('mouseover', (e) => {
-		console.log(e.target)
+		e.target.closest('.about__frame-trigger')
+		if (e.target.closest('.about__frame-trigger')) {
+			const frame = e.target.closest('.about__frame')
+			translateIntoView(frame)
+			frame.classList.add('show')
+			isPaused = true
+			frame.addEventListener('mouseleave', mouseLeaveHandler)
+
+			function mouseLeaveHandler() {
+				frame.classList.remove('show')
+				$sectionAbout.classList.remove('animation')
+
+				setTimeout(() => {
+					isPaused = false
+					setTransform()
+				}, 300)
+				frame.removeEventListener('mouseleave', mouseLeaveHandler)
+			}
+		}
 	})
 
-	setTransform()
-
 	function setTransform() {
+		if (isPaused) return
 		const currentTransformX = $sectionAbout.getBoundingClientRect().x
 		const currentTransformY = $sectionAbout.getBoundingClientRect().y
 
@@ -67,12 +90,33 @@ if (window.location.pathname === '/page-about.html' && !isMobile() && window.inn
 				? (yTransform = yTransform)
 				: (yTransform = currentTransformY + speed * mouseYVelocity)
 		}
-		if (Math.abs(mouseX) < 100 && Math.abs(mouseY) < 100) {
-			xTransform = currentTransformX
-			yTransform = currentTransformY
-		}
+		// if (Math.abs(mouseX) < 200 && Math.abs(mouseY) < 100) {
+		// 	xTransform = currentTransformX
+		// 	yTransform = currentTransformY
+		// }
 		$sectionAbout.style.cssText = `transform: translate(${xTransform}px,${yTransform}px)`
 
 		requestAnimationFrame(setTransform)
+	}
+
+	function translateIntoView(frame) {
+		$sectionAbout.classList.add('animation')
+
+		const frameWidth = frame.offsetWidth
+		const frameHeight = frame.offsetHeight
+		const frameOffsetLeft = frame.offsetLeft
+		const frameOffsetTop = frame.offsetTop
+
+		const frameXCenter = frameOffsetLeft + frameWidth / 2
+		const frameYCenter = frameOffsetTop + frameHeight / 2
+
+		const windowXCenterFS = window.innerWidth / 2
+		const windowYCenterFS = window.innerHeight / 2
+		const diffX = frameXCenter - windowXCenterFS
+		const diffY = frameYCenter - windowYCenterFS
+
+		$sectionAbout.style.cssText = `transform: translate(${-diffX}px,${-diffY}px)`
+
+		isPaused = true
 	}
 }
